9752aebc51c301d9c8e2f14a3795d529476e8975
MoCoXIII <112095580+MoCoXIII@users.noreply.github.com>
2026-02-04 16:42:07 +0100

    add serial port features printing all available ports and automatically switching port names by os

--- rpi.py
+++ rpi.py
@@ -1,11 +1,23 @@
+import os
 import time
 import serial
+
+import serial.tools.list_ports
+
+ports = serial.tools.list_ports.comports()
+for port in ports:
+    print(port.device, "-", port.description)
+
 
 ser = None
 while not ser:
     try:
-        ser = serial.Serial("COM5", 115200, timeout=1)  # Windows
-        # ser = serial.Serial('/dev/ttyACM0', 115200)  # Linux / Pi
+        if os.name == 'nt':
+            ser = serial.Serial("COM5", 115200, timeout=1)  # Windows
+        elif os.name == 'posix':
+            ser = serial.Serial('/dev/ttyACM0', 115200)  # Linux / Pi
+        else:
+            raise Exception("Unsupported OS")
     except Exception as e:
         print(e, "\nRetrying in 10s...")
         time.sleep(10)
